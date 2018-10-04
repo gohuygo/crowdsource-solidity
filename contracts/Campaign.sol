6fd3c9943511c105337416b48ccbcb0f197c9e9a
Huy Nguyen <huy@gohuygo.com>
2018-10-03 19:52:37 -0700

    Add request struct and modifier for restricted access

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -1,9 +1,22 @@
 pragma solidity ^0.4.17;
 
 contract Campaign {
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+    }
+
+    Request[] public requests;
     address public manager;
     uint public minimumContribution;
     address[] public approvers;
+
+    modifier onlyManager() {
+       require(msg.sender == manager);
+       _;
+    }
 
     constructor(uint minimum) public {
        manager = msg.sender;
@@ -15,4 +28,17 @@ contract Campaign {
 
         approvers.push(msg.sender);
     }
+
+    function createRequest(string description, address recipient, uint value)
+        public onlyManager {
+        Request newRequest = Request({
+            description: description,
+            recipient: recipient,
+            value: value,
+            complete: false
+        });
+
+        requests.push(newRequest);
+    }
+
 }
