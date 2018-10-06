74b3c57e7912bc64af0237fd33962a912cdc91be
Huy Nguyen <huy@gohuygo.com>
2018-10-05 18:31:37 -0700

    Styling and refactoring components

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,19 @@
-import React from 'react';
-
+import React, { Fragment } from 'react';
+import { Container }  from 'semantic-ui-react';
+import Head from 'next/head';
+import Header from './Header';
 
 export default(props) => {
   return(
-    <div>
-      <h1> HEADER </h1>
-      {props.children}
-      <h1> FOOTER </h1>
-    </div>
+
+    <Fragment>
+      <Head>
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.3/semantic.min.css"></link>
+      </Head>
+      <Header />
+      <Container>
+        {props.children}
+      </Container>
+    </Fragment>
   )
 };
