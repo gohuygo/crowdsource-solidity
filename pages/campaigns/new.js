84985fa1ffd75a36278deffe3dd587681524a06d
Huy Nguyen <huy@gohuygo.com>
2018-10-05 20:06:22 -0700

    Create campaign on form submit and error messaging

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,18 +1,33 @@
 import React,  { Component } from 'react';
-import { Form, Button, Input } from 'semantic-ui-react';
+import { Form, Button, Input, Message } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
+import factory from '../../ethereum/factory';
+import web3 from '../../ethereum/web3';
 
 class CampaignNew extends Component {
   state = {
     minimumContribution: '',
+    errorMessage: '',
   };
+
+  onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const accounts = await web3.eth.getAccounts()
+      await factory.methods.createCampaign(this.state.minimumContribution)
+                           .send({ from: accounts[0] });
+    } catch (err) {
+      this.setState({errorMessage: err.message})
+    }
+  }
 
   render() {
     return (
       <Layout>
         <h3>Create a New Campaign</h3>
 
-        <Form>
+        <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
           <Form.Field>
             <label>Minimum Contribution</label>
             <Input
@@ -24,6 +39,8 @@ class CampaignNew extends Component {
               }
             />
           </Form.Field>
+
+          <Message error header="Oops!" content={this.state.errorMessage} />
           <Button primary>Create Campaign</Button>
         </Form>
       </Layout>
