ab0889354a96a0b81ca62586190ca9e211852ac5
Huy <huy@gohuygo.com>
2018-11-28 16:12:42 -0800

    Fallback to process.env for heroku

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -6,7 +6,7 @@ let provider;
 if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
   provider = window.web3.currentProvider;
 } else {
-  provider = new Web3.providers.HttpProvider(env.INFURA_API_KEY);
+  provider = new Web3.providers.HttpProvider(env.INFURA_API_KEY || process.env.INFURA_API_KEY);
 }
 
 web3 = new Web3(provider);
