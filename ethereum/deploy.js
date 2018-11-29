941a81f051bd3d2f911675d5d640aa201e30a19e
Huy <huy@gohuygo.com>
2018-11-28 16:16:15 -0800

    Build for heroku

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -1,7 +1,7 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
 const compiledFactory = require('./build/CampaignFactory.json')
-const env = require('../env-config.js');
+// const env = require('../env-config.js');
 
 
 const provider = new HDWalletProvider(
--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,12 +1,13 @@
 import Web3 from 'web3';
-import env from '../env-config.js'
+// import env from '../env-config.js'
 
 let web3;
 let provider;
 if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
   provider = window.web3.currentProvider;
 } else {
-  provider = new Web3.providers.HttpProvider(env.INFURA_API_KEY || process.env.INFURA_API_KEY);
+  // Fallback to process.env for heroku. TODO: Figure out how to switch to process.env
+  provider = new Web3.providers.HttpProvider(process.env.INFURA_API_KEY);
 }
 
 web3 = new Web3(provider);
